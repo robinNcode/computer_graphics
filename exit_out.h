b3de29b64e1b23e39623f79aee5780a2ab220090
robinNcode <msmrobin518@gmail.com>
2022-04-06 00:38:16 +0600

    hut and digital clock added

--- exit_out.h
+++ exit_out.h
@@ -1,11 +1,12 @@
-#include "night_sky.h"
+#include "name.h"
 void exit_output(){
+    setcolor(YELLOW);
+    robin();
+
     setcolor(4);
     outtextxy(30,100,"Press");
     outtextxy(30,130,"any");
     outtextxy(30,160,"key");
     outtextxy(30,190, "to");
     outtextxy(30,220,"Quit");
-
-    nightSky();
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include<graphics.h>
-#include "name.h"
 #include "emoji.h"
 #include "flag.h"
 #include "animated_circle.h"
-#include "bar_chart.h"
 #include "exit_out.h"
-//#include "night_sky.h"
+#include "night_sky.h"
+#include "hut.h"
+#include "clock.h"
 
 using namespace std;
 
@@ -14,7 +14,7 @@ bool stayInProgram = true;
 
 void pattern(string input)
 {
-    cout<<"-----     "<<input<<endl;
+    cout<<"\t\t\t"<<input<<endl;
 }
 
 void invoke(void (*func)())
@@ -35,21 +35,23 @@ int main()
     int x;
     while(stayInProgram)
     {
-        cout<<"Computer Graphics Project"<<endl;
-        pattern("1. My Name");
+        cout<<"\t\t\t<<<<<<<<<< Computer Graphics Project >>>>>>>>>>>"<<endl;
+        cout<<"\t\t\t<<<<<<<<<<       CSE1901016113       >>>>>>>>>>>"<<endl;
+        cout<<"\t\t\t************************************************"<<endl;
+        pattern("1. Digital Clock");
         pattern("2. Flag");
         pattern("3. Emoji");
         pattern("4. Animated Circle");
-        pattern("5. Bar Chart");
+        pattern("5. My Hut");
         pattern("6. Exit");
 
-        pattern("Enter a Number to Get a 2D Graphical Image");
+        cout<<"Enter an option :";
         cin>>x;
 
         switch(x)
         {
         case 1:
-            invoke(&robin);
+            invoke(&digitalClock);
             break;
         case 2:
             invoke(&flag);
@@ -61,7 +63,7 @@ int main()
             invoke(&animated_circle);
             break;
         case 5:
-            invoke(&bar_chart);
+            invoke(&myHut);
             break;
         default:
             stayInProgram = false;
